adf720447804833cd93ff4b503b3e72cd11dfe28
BeyonceM <43378793+BeyonceM@users.noreply.github.com>
2019-03-19 09:39:25 -0400

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,13 @@
+$("button").click(function(){
+      var beyonce= $(".beyonce").val(); 
+          $("p").append("My name is "+beyonce+". ");
+});
+       
+ $("button").click(function(){
+     var color= $(".color").val(); 
+        $("p").append("My favorite color is "+color+". ");
+});
+     $("button").click(function(){
+     var food= $(".food").val(); 
+        $("p").append("My favorite food is "+food+". ");
+});
